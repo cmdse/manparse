83bec7a66a2bc1dd51e52d4423f12195b6d8b38c
Jules Randolph <jules.sam.randolph@gmail.com>
2018-02-18 00:37:47 -0500

    docbook package

---
+++ docbook/man_docbook_xml.go
@@ -0,0 +1,14 @@
+package docbook
+
+import (
+	"github.com/cmdse/manparse/docbook/section"
+	"github.com/cmdse/manparse/docbook/synopsis"
+)
+
+// http://tdg.docbook.org/tdg/4.5/cmdsynopsis.html
+type ManDocBookXml struct {
+	Name        string               `xml:"refnamediv>refname"`
+	Purpose     string               `xml:"refnamediv>refpurpose"`
+	CmdSynopsis synopsis.CmdSynopsis `xml:"refsynopsisdiv>cmdsynopsis"`
+	Sections    []section.Section    `xml:"refsect1"`
+}
